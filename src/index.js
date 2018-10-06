5aaaed26c4898f8a1313650f4e7bbd83aee1b92e
Aliaksei Navamlinets <alex.novomlinets@gmail.com>
2018-10-06 12:43:51 +0300

    feat: implement feature according to the task#12 (7 in 10)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,73 @@
-// DO WHATEVER YOU WANT HERE
+const createEnumerableProperty = (propertyName) => {
+    const objectKeysClone = Object.keys.bind({});
+    Object.keys = (object) =>
+        objectKeysClone(object).filter(key => key === propertyName);
+    return propertyName;
+};
+const createNotEnumerableProperty = (propertyName) => {
+    const objectKeysClone = Object.keys.bind({});
+    Object.keys = (object) =>
+        objectKeysClone(object).filter(key => key !== propertyName);
+    return propertyName;
+};
+const createProtoMagicObject = () => {
+    //return 0;
+};
+const incrementor = () => {   
+    if(global.count === undefined) {global.count = 0;}
+    global.count++;
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+    const inc = () => {
+        global.count++;
+        return inc;
+    }
+
+    inc.valueOf = () => {return global.count;}
+
+    return inc;
+};
+const asyncIncrementor = () => {
+    if(global.asyncCount === undefined) {global.asyncCount = 0;}
+    global.asyncCount++;
+
+    const asyncInc = () => {
+        global.asyncCount++;
+        return inc;
+    }
+
+    asyncInc.valueOf = () => {return global.asyncCount;}
+
+    return asyncInc;
+};
+const createIncrementer = () => {
+    let count = 0,
+        arr = [];
+    while (count !== 6) {arr[count] = ++count;}
+    return arr[Symbol.iterator]();
+};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const getDeepPropertiesCount = (object) => {
+    const getDeepCount = (object) => {
+        let keys = [];
+        for(let key in object) {
+            keys.push(key);
+            if(typeof object[key] === "object") {
+                let subkeys = getDeepCount(object[key]);
+                keys = keys.concat(subkeys.map(function(subkey) {
+                    return key + "." + subkey;
+                }));
+            }
+        }
+        return keys;
+    };
+
+    return getDeepCount(object).length;
+};
+const createSerializedObject = () => {
+    return new String();
+};
 const toBuffer = () => {};
 const sortByProto = () => {};
 
